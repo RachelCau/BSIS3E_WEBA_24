b320069aa89dd2dbb578b64d24a9c21641811ebb
Julianna <yanietorres13@gmail.com>
2024-02-19 16:46:52 +0800

    updatecode .

---
+++ mvc/app/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home
+{
+
+    public function index()
+    {
+        echo 'Home/index';
+    }
+
+    public function test()
+    {
+        echo 'OK!';
+    }
+}
--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    MEOW
-</body>
-</html>
+<?php
+//Note: Php doesn't need a lot of closing  //
+require '../app/core/init.php';
+
+$app = new App();

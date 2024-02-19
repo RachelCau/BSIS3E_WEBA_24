64c15bb0554891e100ab08f2bd264ad0d7711eee
Polka_Roxas_Torres.K_Torres.N <>
2024-02-19 16:55:24 +0800

    updated functions

---
+++ mvc/app/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home
+{
+    public function index()
+    {
+        echo 'Home/Index';
+    }
+
+    public function test()
+    {
+        echo 'Ochige!';
+    }
+}
---
+++ mvc/app/controllers/_404.php
@@ -0,0 +1,14 @@
+<?php
+
+class _404
+{
+    public function index()
+    {
+        echo 'Controller Not Found!';
+    }
+
+    // public function test()
+    // {
+    //     echo 'Ochige!';
+    // }
+}

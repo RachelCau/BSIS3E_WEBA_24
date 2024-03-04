203585c7ab43df7e53dc54380b8868421e36899e
olalia <renatoolalia128@gmail.com>
2024-03-04 16:32:29 +0800

    added model functions and controllers

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -4,6 +4,11 @@ class Home extends Controller
 {
     public function index()
     {
+        $model = new Model();
+        $arr['lastname'] = 'user';
+        $data = $model->where($arr);
+        show($data);
+
         $this->view('home');
     }
 }

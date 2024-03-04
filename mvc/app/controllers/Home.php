33ec9b003801f0e6a6732cd5f932a83a40a4cb12
cucharo <patriziapipperzoe@gmail.com>
2024-03-04 16:40:19 +0800

    update03_04

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -4,6 +4,11 @@ class Home extends Controller
 {
     public function index()
     {
+        $model = new Model();
+        $arr['lastname'] = ' Cucharo';
+        $data = $model->where($arr);
+        show($data);
+        
         $this->view('home');
     } 
     

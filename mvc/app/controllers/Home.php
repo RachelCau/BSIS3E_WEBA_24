85cbf384bbd945c4e5c43705389fd22f2fb8f82a
Julianna <yanietorres13@gmail.com>
2024-03-18 16:20:48 +0800

    MEOWDATE

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -5,11 +5,12 @@ class Home extends Controller
     public function index()
     {   
         $user = new User();
-        $arr['firstname'] = 'Julianna'; 
-        $arr['lastname'] = 'Torres'; 
-        $arr['email'] = 'yanie@gmailcom'; 
-        $arr['password'] = '1234'; 
-        $data = $user->update('2',$arr);
+        $arr['firstname'] = 'Renato Jr.'; 
+        $arr['lastname'] = 'Ren'; 
+       // $arr['email'] = 'ren@gmailcom'; 
+        //$arr['password'] = '1234'; 
+        $data = $user->update('1',$arr);
+        $data = $user->findAll();
         show($data);
 
         $this->view('home');

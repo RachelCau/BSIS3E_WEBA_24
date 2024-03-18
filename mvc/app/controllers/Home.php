1a0d39f45285e4556a6fde04c4c72b4b6d4cb49e
cucharo <patriziapipperzoe@gmail.com>
2024-03-18 16:22:09 +0800

    added new functions

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -5,8 +5,11 @@ class Home extends Controller
     public function index()
     {
         $model = new Model();
-        $arr['lastname'] = ' Cucharo';
-        $data = $model->where($arr);
+        $arr[' firstname'] = 'John';
+        $arr['lastname'] = 'Doe';
+        $arr['email'] = 'jd@gmail.com';
+        $arr['password'] = '1234';
+        $data = $user->update($arr);
         show($data);
         
         $this->view('home');

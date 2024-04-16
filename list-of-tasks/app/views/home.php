72ee1f9d70b8b80b476553f0b5c5fd6a5dbd3c91
Olalia <renatoolalia128@gmail.com>
2024-04-16 12:36:32 +0800

    FINISHED Activity

---
+++ list-of-tasks/app/views/home.php
@@ -0,0 +1,9 @@
+<?php include "partials/header.php" ?>
+
+<div class="container mt-5 d-flex justify-content-center">
+      <h1>Renato Jr. Olalia</h1>
+</div>
+<div class="container mt-5 d-flex justify-content-center">
+      <h1>Activity in WEBA24</h1>
+</div>
+<?php include "partials/footer.php" ?>
